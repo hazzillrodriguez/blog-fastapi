7a1685882da72f768d689eb1297d074d123321a9
hazzillrodriguez <hazzillrodriguez@gmail.com>
2022-07-26 21:59:32 +0800

    cross-origin resource sharing

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from .routers import auth, user, post, vote
 from . import models
 from .database import engine
@@ -7,6 +9,14 @@ from .config import settings
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
 app.include_router(user.router)
