f98d6d250d97027575983f641baa23aee58d2d7f
hazzillrodriguez <hazzillrodriguez@gmail.com>
2022-07-27 00:50:45 +0800

    OAuth2 PasswordRequestForm

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 from .. import schemas, models, oauth2
@@ -8,8 +9,8 @@ from ..utils.password import verify_password
 router = APIRouter(tags=['Auth'])
 
 @router.post('/login')
-def login(login: schemas.Login, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.email == login.email).first()
+def login(login: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.email == login.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
             detail='E-mail address does not exist!')
